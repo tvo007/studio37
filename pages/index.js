38f9215dc8e521bd0641e0879b9b5a76fd9a1eb6
Tim Vo <timtvo714@gmail.com>
2022-06-30 11:50:42 -0700

    moved components folder to src

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,21 @@
 
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import {ServicesSection, ContactSection} from './components';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import {ServicesSection, ContactSection} from '../components';
 
 export default function Home () {
   return (
     <div>
 
       <Hero />
-      {/* <Nav /> */}
+
 
 
       <ServicesSection />
 
-      {/**content3 */}
+     
 
-      {<ContactSection />}
-      {/* <ContactOld /> */}
-      {/* <Content2 /> */}
-
-      {/**content 1 */}
-      {/* <Content3 /> */}
-      {/**footer */}
+      <ContactSection />
       <Footer />
 
     </div>
